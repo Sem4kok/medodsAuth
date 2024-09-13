32b202c3da99181848db329f1cad7da4e4436096
Sem4kok <w1usis@mail.ru>
2024-09-14 01:46:15 +0300

    feat(user): user controller's added

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,13 +1,19 @@
 package app
 
 import (
+	"context"
 	"github.com/gin-gonic/gin"
 	"log"
 	"medodsAuth/internal/controller/users"
+	storage "medodsAuth/internal/storage/postgresql"
+	"net/http"
+	"os/signal"
+	"syscall"
 )
 
 const (
-	localhost = ":8081"
+	localhost   = ":8081"
+	storagePath = ""
 )
 
 type App struct {
@@ -16,13 +22,30 @@ type App struct {
 
 func New() *App {
 	return &App{
-		gin.Default(),
+		Engine: gin.Default(),
 	}
 }
 
 func (app *App) MustRunApp() {
+	quit, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGABRT, syscall.SIGKILL,
+		syscall.SIGALRM)
+	defer stop()
+
+	server := http.Server{
+		Handler: app.Engine,
+	}
+
+	storage.ConnectDB(storagePath)
 	app.handleUrls()
-	log.Fatal(app.Run(localhost))
+
+	go func() {
+		log.Fatal(app.Run(localhost))
+	}()
+
+	<-quit.Done()
+	log.Println("receive interrupt signal")
+
+	server.Close()
 }
 
 func (app *App) handleUrls() {
--- internal/controller/users/user.go
+++ internal/controller/users/user.go
@@ -3,14 +3,26 @@ package users
 import (
 	"github.com/gin-gonic/gin"
 	"medodsAuth/internal/models"
+	storage "medodsAuth/internal/storage/postgresql"
 	"net/http"
 )
 
+// TODO HANDLERS
 func Register(c *gin.Context) {
 	var user *models.User
 	if err := c.ShouldBindJSON(&user); err != nil {
-		c.JSON(http.StatusInternalServerError, err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 	}
+
+	if err := user.CreateUser(); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	}
+
+	if err := storage.DB.SaveUser(user); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "User created successfully"})
 }
 
 func Login(c *gin.Context) {
