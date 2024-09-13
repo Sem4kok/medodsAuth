9585276fed938115d5e789d6a5c25822faed5343
Sem4kok <w1usis@mail.ru>
2024-09-14 01:44:59 +0300

    refactor(user): user model uuid added

--- internal/models/user.go
+++ internal/models/user.go
@@ -2,18 +2,20 @@ package models
 
 import (
 	"fmt"
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
 	"strings"
 )
 
 type User struct {
-	ID        int    `json:"ID"`
+	GUID      string `json:"GUID"`
 	FirstName string `json:"first_name"`
 	LastName  string `json:"last_name"`
 	Email     string `json:"email"`
 	Password  string `json:"password"`
 }
 
-func (user *User) Validate() error {
+func (user *User) validate() error {
 	user.FirstName = strings.TrimSpace(user.FirstName)
 	user.LastName = strings.TrimSpace(user.LastName)
 
@@ -26,6 +28,24 @@ func (user *User) Validate() error {
 	if user.Password == "" {
 		return fmt.Errorf("invalid password")
 	}
+
+	return nil
+}
+
+// CreateUser encrypts user password and validates a data
+func (user *User) CreateUser() error {
+	if err := user.validate(); err != nil {
+		return err
+	}
+
+	pwSlice, err := bcrypt.GenerateFromPassword([]byte(user.Password), 14)
+	if err != nil {
+		return fmt.Errorf("failed to encrypt the password")
+	}
+	user.Password = string(pwSlice)
+
+	// TODO guid for user
+	user.GUID = uuid.New().String()
 
 	return nil
 }
