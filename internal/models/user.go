d9a0f1bb3d5ca6811fd0feaf275b98823912ae0a
Sem4kok <w1usis@mail.ru>
2024-09-13 12:58:57 +0300

    feat(user): user validation added

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,9 +1,31 @@
 package models
 
+import (
+	"fmt"
+	"strings"
+)
+
 type User struct {
-	ID         int
-	FirstName  string
-	SecondName string
-	Email      string
-	Password   string
+	ID        int    `json:"ID"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+}
+
+func (user *User) Validate() error {
+	user.FirstName = strings.TrimSpace(user.FirstName)
+	user.LastName = strings.TrimSpace(user.LastName)
+
+	user.Email = strings.TrimSpace(user.Email)
+	if user.Email == "" {
+		return fmt.Errorf("invalid email")
+	}
+
+	user.Password = strings.TrimSpace(user.Password)
+	if user.Password == "" {
+		return fmt.Errorf("invalid password")
+	}
+
+	return nil
 }
