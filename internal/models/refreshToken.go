843c9cd3286bc2d061118269f0e17d1c7328f2ae
Sem4kok <w1usis@mail.ru>
2024-09-14 23:29:18 +0300

    feat(models): tokens added

---
+++ internal/models/refreshToken.go
@@ -0,0 +1,8 @@
+package models
+
+type RefreshToken struct {
+	RefreshTokenHash string
+	GUID             string
+	IP               string
+	ID               string
+}
---
+++ internal/models/tokens.go
@@ -0,0 +1,8 @@
+package models
+
+type Tokens struct {
+	Access           string
+	Refresh          string
+	RefreshTokenHash string
+	TokenID          string
+}
